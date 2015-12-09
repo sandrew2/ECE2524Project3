4a3356bb42596ed68b37f474bc599f6365b5bd4c
sandrew2 <sandrew2@vt.edu>
2015-12-08 20:15:15 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -2,20 +2,40 @@
 
 from Tkinter import *
 
-#create the window
-gui = Tk()
+class Application(Frame):
+
+    def __init__(self, master):
+        Frame.__init__(self,master)
+        self.grid()
+        self.create_widgets()
+
+    def create_widgets(self):
+        #Create First Label
+        self.label1 = Label(self, text = "Welcome to the Mini-Game Console!")
+        self.label1.grid()
+
+        #Create Second Label
+        self.label2 = Label(self, text = "Enter your name then press start to begin!")
+        self.label2.grid(row=1)
+
+        #Create Third Label
+        self.label2 = Label(self, text = "Enter Name Here:")
+        self.label2.grid(row=2, sticky=W)
+
+        #Create Name Entry Box
+        self.en1 = Entry(self)
+        self.en1.grid(row=2, sticky=E)
+
+        #Create Start Button
+        self.button1 = Button(self, text = "START")
+        self.button1.grid(row=3)
 
 #modift root window
+gui = Tk()
 gui.title("GameConsole")
 gui.geometry("230x200")
 
-app = Frame(gui)
-app.grid()
-Label(gui, text = "Welcome to the Mini-Game Console!").grid(row=0)
-Label(gui, text = "Enter your name then press start to begin!").grid(row=1)
-Label(gui, text = "Enter Name Here:").grid(row=2, sticky=W)
-Entry(gui).grid(row=2, sticky=E)
-Button(gui, text = "START").grid(row=3)
+app = Application(gui)
 
 #kick off the event loop
 gui.mainloop()
