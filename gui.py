a5bc589e8e8eadd87cf9fe7974d596a2f5994127
sandrew2 <sandrew2@vt.edu>
2015-12-08 21:41:21 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -32,14 +32,100 @@ class Application(Frame):
 
     def next_screen(self):
         #Save input name
-        user_name = self.en1.get()
+        self.user_name = StringVar()
+        self.user_name = self.en1.get()
+        #Clear welcome screen
         self.label1.destroy()
         self.label2.destroy()
         self.label3.destroy()
         self.en1.destroy()
         self.button1.destroy()
+        #Resize the frame
         gui.geometry("500x500")
-        
+        #Move to the main menu
+        self.main_menu()
+
+    def main_menu(self):
+        self.label4 = Label(self, text = "Main Menu", font = 38, fg="red")
+        self.label4.grid(row=0)
+
+        self.label5 = Label(self, text ="Welcome to the fun")
+        self.label5.grid(row=1)
+
+        self.label6 = Label(self, textvariable = self.user_name)
+        self.label6.grid(row=2)
+
+        #Hangman BTN and description
+        self.hangman_button = Button(self, text = "START", command = self.hangman)
+        self.hangman_button.grid(row=3)
+
+        self.hangman_desc = Label(self, text = "Play the classic game of hangman versus a computer! You have 7 tries")
+        self.hangman_desc.grid(row=3, column=1, sticky=W)
+
+        self.hangman_desc_cont = Label(self, text = "to guess the word before the man hangs!")
+        self.hangman_desc_cont.grid(row=4, column=1, sticky=W)
+
+        #Tic-Tac-Toe BTN and description
+        self.tictactoe_button = Button(self, text = "START", command = self.tictactoe)
+        self.tictactoe_button.grid(row=5)
+
+        self.tictactoe_desc = Label(self, text = "Play the classic game of tic-tac-toe versus a computer! Choose to be")
+        self.tictactoe_desc.grid(row=5, column=1, sticky=W)
+
+        self.tictactoe_desc_cont = Label(self, text = "X's or O's and see if you can best the computer!")
+        self.tictactoe_desc_cont.grid(row=6, column=1, sticky=W)
+
+        #Connect4 BTN and description
+        self.connect4_button = Button(self, text = "START", command = self.connect4)
+        self.connect4_button.grid(row=7)
+
+        self.connect4_desc = Label(self, text = "Play the classic game of Connect Four versus a computer! Choose to be")
+        self.connect4_desc.grid(row=7, column=1, sticky=W)
+
+        self.connect4_desc_cont = Label(self, text = "Red or Black and see if you can best the computer!")
+        self.connect4_desc_cont.grid(row=8, column=1, sticky=W)
+
+    def hangman(self):
+        self.label4.destroy()
+        self.label5.destroy()
+        self.label6.destroy()
+        self.hangman_button.destroy()
+        self.hangman_desc.destroy()
+        self.hangman_desc_cont.destroy()
+        self.tictactoe_button.destroy()
+        self.tictactoe_desc.destroy()
+        self.tictactoe_desc_cont.destroy()
+        self.connect4_button.destroy()
+        self.connect4_desc.destroy()
+        self.connect4_desc_cont.destroy()
+
+    def tictactoe(self):
+        self.label4.destroy()
+        self.label5.destroy()
+        self.label6.destroy()
+        self.hangman_button.destroy()
+        self.hangman_desc.destroy()
+        self.hangman_desc_cont.destroy()
+        self.tictactoe_button.destroy()
+        self.tictactoe_desc.destroy()
+        self.tictactoe_desc_cont.destroy()
+        self.connect4_button.destroy()
+        self.connect4_desc.destroy()
+        self.connect4_desc_cont.destroy()
+
+    def connect4(self):
+        self.label4.destroy()
+        self.label5.destroy()
+        self.label6.destroy()
+        self.hangman_button.destroy()
+        self.hangman_desc.destroy()
+        self.hangman_desc_cont.destroy()
+        self.tictactoe_button.destroy()
+        self.tictactoe_desc.destroy()
+        self.tictactoe_desc_cont.destroy()
+        self.connect4_button.destroy()
+        self.connect4_desc.destroy()
+        self.connect4_desc_cont.destroy()
 
 #modify root window
 gui = Tk()
