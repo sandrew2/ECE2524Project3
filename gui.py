c6c87b9ef678ded21d02d8eda247bfdfaab2a789
sandrew2 <sandrew2@vt.edu>
2015-12-08 19:57:54 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -7,15 +7,14 @@ gui = Tk()
 
 #modift root window
 gui.title("GameConsole")
-gui.geometry("225x200")
+gui.geometry("230x200")
 
 app = Frame(gui)
 app.grid()
 Label(gui, text = "Welcome to the Mini-Game Console!").grid(row=0)
 Label(gui, text = "Enter your name then press start to begin!").grid(row=1)
-Label(gui, text = "Your Name:").grid(row=2, column=0)
-name = Entry(gui)
-name.grid(row=2, column=1)
+Label(gui, text = "Enter Name Here:").grid(row=2, sticky=W)
+Entry(gui).grid(row=2, sticky=E)
 Button(gui, text = "START").grid(row=3)
 
 #kick off the event loop
