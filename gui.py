177dc45d03a58e67408378f5a3c891cebbddf3c4
sandrew2 <sandrew2@vt.edu>
2015-12-08 20:44:24 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -19,18 +19,29 @@ class Application(Frame):
         self.label2.grid(row=1)
 
         #Create Third Label
-        self.label2 = Label(self, text = "Enter Name Here:")
-        self.label2.grid(row=2, sticky=W)
+        self.label3 = Label(self, text = "Enter Name Here:")
+        self.label3.grid(row=2, sticky=W)
 
         #Create Name Entry Box
         self.en1 = Entry(self)
         self.en1.grid(row=2, sticky=E)
 
         #Create Start Button
-        self.button1 = Button(self, text = "START")
+        self.button1 = Button(self, text = "START", command = self.next_screen)
         self.button1.grid(row=3)
 
-#modift root window
+    def next_screen(self):
+        #Save input name
+        user_name = self.en1.get()
+        self.label1.destroy()
+        self.label2.destroy()
+        self.label3.destroy()
+        self.en1.destroy()
+        self.button1.destroy()
+        gui.geometry("500x500")
+        
+
+#modify root window
 gui = Tk()
 gui.title("GameConsole")
 gui.geometry("230x200")
