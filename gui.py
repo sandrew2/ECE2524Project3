be90c9b1b7232ad100e0efb0662ddf231670fa9d
sandrew2 <sandrew2@vt.edu>
2015-12-08 19:12:00 -0500

    Create gui.py

---
+++ gui.py
@@ -0,0 +1,13 @@
+#Main GUI
+
+from Tkinter import *
+
+#create the window
+root = Tk()
+
+#modift root window
+root.title("GameConsole")
+root.geometry("500x900")
+
+#kick off the event loop
+root.mainloop()
